8a48caa36ed7ee22e0425c255479e4ae3bbecfea
d064310 <andreas.pfau@sap.com>
2024-11-04 12:53:53 +0100

    chore(core): initial commit

---
+++ apps/dashboard/src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function App() {
+  return (
+    <div>
+      <h1>Aurora Dashboard</h1>
+      <p>Welcome to the Aurora Dashboard!</p>
+    </div>
+  )
+}
