118e75a13d13563d8adde82007e3eee31e36253b
d064310 <andreas.pfau@sap.com>
2024-11-08 18:30:33 +0100

    chore(ci): remove apps/template

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import App from "./components/App"
 import "./style.css"
 
-const container = document.getElementById("app") as HTMLDivElement
-const root = createRoot(container)
+const container = document.getElementById("app")
+const root = createRoot(container!)
 
 root.render(<App />)
--- apps/template/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client"
-import React from "react"
-import App from "./components/App"
-import "./style.css"
-
-const container = document.getElementById("app") as HTMLDivElement
-const root = createRoot(container)
-
-root.render(<App />)
