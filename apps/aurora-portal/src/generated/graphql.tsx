79cbb0ff6bd0b86352c6f0bc2983cb7424c6b5e7
d064310 <andreas.pfau@sap.com>
2024-11-28 13:44:38 +0100

    chore(core): remove tsconfig.tsbuildinfo from git

--- apps/aurora-portal/src/generated/graphql.tsx
+++ apps/aurora-portal/src/generated/graphql.tsx
@@ -1,72 +1,80 @@
-import { gql } from '@apollo/client';
-import * as Apollo from '@apollo/client';
-export type Maybe<T> = T | null;
-export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
-export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
-const defaultOptions = {} as const;
+import { gql } from "@apollo/client"
+import * as Apollo from "@apollo/client"
+export type Maybe<T> = T | null
+export type InputMaybe<T> = Maybe<T>
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] }
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> }
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> }
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never }
+export type Incremental<T> = T | { [P in keyof T]?: P extends " $fragmentName" | "__typename" ? T[P] : never }
+const defaultOptions = {} as const
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: { input: string; output: string; }
-  String: { input: string; output: string; }
-  Boolean: { input: boolean; output: boolean; }
-  Int: { input: number; output: number; }
-  Float: { input: number; output: number; }
-};
+  ID: { input: string; output: string }
+  String: { input: string; output: string }
+  Boolean: { input: boolean; output: boolean }
+  Int: { input: number; output: number }
+  Float: { input: number; output: number }
+}
 
 export type Author = {
-  __typename?: 'Author';
-  id: Scalars['ID']['output'];
+  __typename?: "Author"
+  id: Scalars["ID"]["output"]
   /** Author's first and last name */
-  name: Scalars['String']['output'];
+  name: Scalars["String"]["output"]
   /** Author's profile picture */
-  photo?: Maybe<Scalars['String']['output']>;
-};
+  photo?: Maybe<Scalars["String"]["output"]>
+}
 
 export type Query = {
-  __typename?: 'Query';
-  tracksForHome: Array<Track>;
-};
+  __typename?: "Query"
+  tracksForHome: Array<Track>
+}
 
 export type Track = {
-  __typename?: 'Track';
+  __typename?: "Track"
   /** The track's main Author */
-  author: Author;
+  author: Author
   /** The track's description */
-  description: Scalars['String']['output'];
-  id: Scalars['ID']['output'];
+  description: Scalars["String"]["output"]
+  id: Scalars["ID"]["output"]
   /** The track's approximate length in minutes */
-  length?: Maybe<Scalars['Int']['output']>;
+  length?: Maybe<Scalars["Int"]["output"]>
   /** The number of modules this track contains */
-  modulesCount?: Maybe<Scalars['Int']['output']>;
+  modulesCount?: Maybe<Scalars["Int"]["output"]>
   /** The track's illustration */
-  thumbnail?: Maybe<Scalars['String']['output']>;
+  thumbnail?: Maybe<Scalars["String"]["output"]>
   /** The track's title */
-  title?: Maybe<Scalars['String']['output']>;
-};
+  title?: Maybe<Scalars["String"]["output"]>
+}
 
-export type GetTracksQueryVariables = Exact<{ [key: string]: never; }>;
+export type GetTracksQueryVariables = Exact<{ [key: string]: never }>
 
-
-export type GetTracksQuery = { __typename?: 'Query', tracksForHome: Array<{ __typename?: 'Track', id: string, title?: string | null, length?: number | null, modulesCount?: number | null, author: { __typename?: 'Author', name: string } }> };
-
+export type GetTracksQuery = {
+  __typename?: "Query"
+  tracksForHome: Array<{
+    __typename?: "Track"
+    id: string
+    title?: string | null
+    length?: number | null
+    modulesCount?: number | null
+    author: { __typename?: "Author"; name: string }
+  }>
+}
 
 export const GetTracksDocument = gql`
-    query GetTracks {
-  tracksForHome {
-    id
-    title
-    author {
-      name
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        name
+      }
+      length
+      modulesCount
     }
-    length
-    modulesCount
   }
-}
-    `;
+`
 
 /**
  * __useGetTracksQuery__
@@ -84,18 +92,22 @@ export const GetTracksDocument = gql`
  * });
  */
 export function useGetTracksQuery(baseOptions?: Apollo.QueryHookOptions<GetTracksQuery, GetTracksQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<GetTracksQuery, GetTracksQueryVariables>(GetTracksDocument, options);
-      }
-export function useGetTracksLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetTracksQuery, GetTracksQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<GetTracksQuery, GetTracksQueryVariables>(GetTracksDocument, options);
-        }
-export function useGetTracksSuspenseQuery(baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<GetTracksQuery, GetTracksQueryVariables>) {
-          const options = baseOptions === Apollo.skipToken ? baseOptions : {...defaultOptions, ...baseOptions}
-          return Apollo.useSuspenseQuery<GetTracksQuery, GetTracksQueryVariables>(GetTracksDocument, options);
-        }
-export type GetTracksQueryHookResult = ReturnType<typeof useGetTracksQuery>;
-export type GetTracksLazyQueryHookResult = ReturnType<typeof useGetTracksLazyQuery>;
-export type GetTracksSuspenseQueryHookResult = ReturnType<typeof useGetTracksSuspenseQuery>;
-export type GetTracksQueryResult = Apollo.QueryResult<GetTracksQuery, GetTracksQueryVariables>;
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useQuery<GetTracksQuery, GetTracksQueryVariables>(GetTracksDocument, options)
+}
+export function useGetTracksLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<GetTracksQuery, GetTracksQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useLazyQuery<GetTracksQuery, GetTracksQueryVariables>(GetTracksDocument, options)
+}
+export function useGetTracksSuspenseQuery(
+  baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<GetTracksQuery, GetTracksQueryVariables>
+) {
+  const options = baseOptions === Apollo.skipToken ? baseOptions : { ...defaultOptions, ...baseOptions }
+  return Apollo.useSuspenseQuery<GetTracksQuery, GetTracksQueryVariables>(GetTracksDocument, options)
+}
+export type GetTracksQueryHookResult = ReturnType<typeof useGetTracksQuery>
+export type GetTracksLazyQueryHookResult = ReturnType<typeof useGetTracksLazyQuery>
+export type GetTracksSuspenseQueryHookResult = ReturnType<typeof useGetTracksSuspenseQuery>
+export type GetTracksQueryResult = Apollo.QueryResult<GetTracksQuery, GetTracksQueryVariables>
