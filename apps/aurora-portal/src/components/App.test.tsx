9c415cc068218d60fa93bdb6be4aa0877737a1f9
Wowa <vladimir.barsukov@sap.com>
2024-11-28 07:36:36 +0100

    chore(dashboard): add proper apollo tests

--- apps/aurora-portal/src/components/App.test.tsx
+++ apps/aurora-portal/src/components/App.test.tsx
@@ -1,15 +1,43 @@
 import React from "react"
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
+import { MockedProvider } from "@apollo/client/testing"
 import { describe, it, expect } from "vitest"
 import App from "./App"
+import { GetTracksDocument } from "../generated/graphql"
+const mocks = [
+  {
+    request: {
+      query: GetTracksDocument,
+    },
+    result: {
+      data: {
+        tracksForHome: [
+          {
+            id: "1",
+            title: "Astro Kitty, Space Explorer",
+            author: { name: "Grumpy Cat", photo: "photo_url" },
+            thumbnail: "thumbnail_url",
+            length: 1210,
+            modulesCount: 6,
+          },
+        ],
+      },
+    },
+  },
+]
 
 describe("App Component", () => {
-  it("renders AppShellProvider and AppShell correctly", () => {
-    render(<App />)
+  it("renders AppShellProvider and AppShell correctly", async () => {
+    render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <App />
+      </MockedProvider>
+    )
+    expect(screen.getByText(/Loading.../i)).toBeInTheDocument()
 
-    const title = screen.getAllByText("Aurora Dashboard")
-
-    // Check that the page header text is present
-    expect(title?.[0]).toBeInTheDocument()
+    // Wait for the data to appear
+    await waitFor(() => {
+      expect(screen.getByText(/Astro Kitty, Space Explorer/i)).toBeInTheDocument()
+    })
   })
 })
