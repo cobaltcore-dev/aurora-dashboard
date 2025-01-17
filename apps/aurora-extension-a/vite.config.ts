9a09d09fb5cb2e26a779ac8d5a913fad69297981
Wowa <vladimir.barsukov@sap.com>
2025-01-17 18:13:22 +0000

    chore(core): generate index.d.ts

--- apps/aurora-extension-a/vite.config.ts
+++ apps/aurora-extension-a/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "tailwindcss"
 import autoprefixer from "autoprefixer"
 import tsconfigPaths from "vite-tsconfig-paths"
 import svgr from "vite-plugin-svgr"
+import dts from "vite-plugin-dts"
 import * as dotenv from "dotenv"
 
 dotenv.config()
@@ -17,7 +18,6 @@ export default defineConfig({
     rollupOptions: {
       external: ["react", "react-dom"],
     },
-
     lib: {
       entry: "src/client/App.tsx", // or 'src/main.ts' if TypeScript
       name: "aurora-extension-a", // Replace with your library's global name
@@ -29,7 +29,18 @@ export default defineConfig({
   define: {
     BFF_ENDPOINT: JSON.stringify(BFF_ENDPOINT),
   },
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    tsconfigPaths(),
+    dts({
+      rollupTypes: true, // Enables type bundling
+      insertTypesEntry: true, // Ensures a types entry is added to package.json
+      outDir: "dist/client",
+      logLevel: "warn",
+      tsconfigPath: "./config/tsconfig.client.json",
+    }),
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
