8bc597ed00c25653e519ec5a0272700f7da2ca75
d064310 <andreas.pfau@sap.com>
2025-01-21 13:29:38 +0100

    chore(core): use fastify/vite to run vite in fastify

--- apps/aurora-portal/src/polaris-bff/generated/extensions.ts
+++
@@ -1,5 +0,0 @@
-import * as server0 from "@cobaltcore-dev/aurora-extension-a/server"
-
-export const registerServers = () => ({
-  "cobaltcore-dev_aurora-extension-a": server0.registerRouter().appRouter,
-})
