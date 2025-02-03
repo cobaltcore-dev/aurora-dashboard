25f8f6c1ae3fbd5660c70cb68156596d6d2edd37
Wowa <vladimir.barsukov@sap.com>
2025-02-03 13:34:22 +0100

    chore(core): clean up

--- apps/legacy-polaris-bff/src/resolvers.ts
+++
@@ -1,6 +0,0 @@
-import { NonEmptyArray } from "type-graphql"
-import { ServerResolver } from "./Compute/resolvers/ServerResolver"
-import { TokenResolver } from "./Identity/resolvers/TokenResolver"
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-export const resolvers: NonEmptyArray<Function> = [TokenResolver, ServerResolver]
