7fafd71e888df58e9838b2126f9af5b9c3393560
d064310 <andreas.pfau@sap.com>
2025-02-14 17:23:24 +0100

    chore(core): fix deps and build

--- apps/aurora-portal/src/server/routers.ts
+++ apps/aurora-portal/src/server/routers.ts
@@ -5,12 +5,16 @@ import { auroraRouter, mergeRouters } from "./trpc"
 
 const coreRouter = mergeRouters(auroraRouter(identityRouters), auroraRouter(computeRouters))
 
-const extensionRouters = Object.fromEntries(
-  Object.entries(serverExtensions).map(([key, value]) => [key, value.appRouter])
-) as {
-  [K in keyof typeof serverExtensions]: (typeof serverExtensions)[K]["appRouter"]
+export let appRouter = coreRouter
+
+if (serverExtensions !== undefined && Object.keys(serverExtensions).length > 0) {
+  const extensionRouters = Object.fromEntries(
+    // @ts-expect-error Object entries
+    Object.entries(serverExtensions).map(([key, value]) => [key, value.appRouter])
+  ) as {
+    [K in keyof typeof serverExtensions]: (typeof serverExtensions)[K]["appRouter"]
+  }
+
+  appRouter = mergeRouters(coreRouter, auroraRouter(extensionRouters))
 }
-
-export const appRouter = mergeRouters(coreRouter, auroraRouter(extensionRouters))
-
 export type AuroraRouter = typeof appRouter
