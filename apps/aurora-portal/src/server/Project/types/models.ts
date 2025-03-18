6ef5bb2d9767493775b84dd9a0dd5d423193c18e
TilmanHaupt <tilman.haupt@sap.com>
2025-03-18 17:01:16 +0100

    chore(core): add basic, validated type für project

--- apps/aurora-portal/src/server/Project/types/models.ts
+++ apps/aurora-portal/src/server/Project/types/models.ts
@@ -1,19 +1,38 @@
-export type Project = {
-  domain_id: string
-  enabled: boolean
-  id: string
-  links: {
-    self: string
-  }
-  name: string
-  description: string
-}
+import { z } from "zod"
+// Define the basic project schema
+export const baseProjectSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  enabled: z.boolean(),
+  domain_id: z.string().nullable().optional(),
+  parent_id: z.string().nullable().optional(),
+  is_domain: z.boolean().nullable().optional(),
+  tags: z.array(z.string()).nullable().optional(),
+  options: z.record(z.string()).nullable().optional(),
+  description: z.string().nullable().optional(),
+  links: z.object({
+    self: z.string().url(),
+  }),
+})
 
-export type ProjectsResponse = {
-  projects: Project[]
-  links: {
-    self: string
-    previous: string | null
-    next: string | null
-  }
-}
+export const projectSchema = baseProjectSchema.extend({
+  parents: z
+    .array(
+      z.object({
+        project: baseProjectSchema,
+      })
+    )
+    .optional(),
+})
+
+export const projectsResponseSchema = z.object({
+  projects: z.array(projectSchema),
+  links: z.object({
+    self: z.string().url(),
+    previous: z.string().url().nullable(),
+    next: z.string().url().nullable(),
+  }),
+})
+
+export type Project = z.infer<typeof projectSchema>
+export type ProjectsResponse = z.infer<typeof projectsResponseSchema>
