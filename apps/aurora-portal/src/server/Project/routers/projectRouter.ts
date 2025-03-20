bb413f12d761e49be6b29525ec6effb7322ad585
TilmanHaupt <tilman.haupt@sap.com>
2025-03-20 18:13:55 +0100

    chore(core): deleted comments in projectRouter

--- apps/aurora-portal/src/server/Project/routers/projectRouter.ts
+++ apps/aurora-portal/src/server/Project/routers/projectRouter.ts
@@ -33,29 +33,3 @@ export const projectRouter = {
       return parsedData.data.projects.find((project: Project) => project.id === input.id)
     }),
 }
-
-// import { z } from "zod"
-// import { protectedProcedure } from "../../trpc"
-// import { Project, projectsResponseSchema } from "../types/models"
-
-// export const projectRouter = {
-//   authProjects: protectedProcedure.query(async ({ ctx }): Promise<Project[] | undefined> => {
-//     const openstackSession = ctx.openstack
-//     console.log("dfssdffdsdsfsdf")
-//     const identityService = openstackSession?.service("identity")
-//     const data = await identityService?.get("projects")
-//     //.then((res) => projectsResponseSchema.safeParse(res.json()))
-//     console.log("Projects", data)
-//     return data?.projects
-//   }),
-//   // temporary router for simple getProjectById should be replaced by rescope
-//   getProjectById: protectedProcedure
-//     .input(z.object({ id: z.string() }))
-//     .query(async ({ input, ctx }): Promise<Project> => {
-//       const openstackSession = ctx.openstack
-
-//       const identityService = openstackSession?.service("identity")
-//       const data = await identityService?.get("projects").then((res) => res.json())
-//       return data.projects.find((project: Project) => project.id === input.id)
-//     }),
-// }
