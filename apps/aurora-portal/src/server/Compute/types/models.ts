7919cb82c79f54c2466d16ae302d40d6a53b4775
TilmanHaupt <tilman.haupt@sap.com>
2025-03-21 12:02:51 +0100

    chore(core): refactor servertype to zod

--- apps/aurora-portal/src/server/Compute/types/models.ts
+++ apps/aurora-portal/src/server/Compute/types/models.ts
@@ -1,26 +1,32 @@
-export interface Server {
-  id: string
-  name: string
-  accessIPv4: string
-  accessIPv6: string
-  addresses: {
-    private: {
-      addr: string
-      mac_addr?: string
-      type?: string
-      version?: number
-    }[]
-  }
-  created: string
-  updated: string
-  status: "ACTIVE" | "SHUTOFF"
-  flavor: {
-    disk: number
-    ram: number
-    vcpus: number
-  }
-  image: {
-    id: string
-  }
-  metadata: Record<string, string>
-}
+import { z } from "zod"
+
+export const serverSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  accessIPv4: z.string(),
+  accessIPv6: z.string(),
+  addresses: z.object({
+    private: z.array(
+      z.object({
+        addr: z.string(),
+        mac_addr: z.string().optional(),
+        type: z.string().optional(),
+        version: z.number().optional(),
+      })
+    ),
+  }),
+  created: z.string(),
+  updated: z.string(),
+  status: z.union([z.literal("ACTIVE"), z.literal("SHUTOFF")]),
+  flavor: z.object({
+    disk: z.number(),
+    ram: z.number(),
+    vcpus: z.number(),
+  }),
+  image: z.object({
+    id: z.string(),
+  }),
+  metadata: z.record(z.string()),
+})
+
+export type Server = z.infer<typeof serverSchema>
