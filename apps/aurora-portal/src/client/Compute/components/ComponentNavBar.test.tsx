56df977a26bfbe09b753fa9d623d6355d42efaf5
TilmanHaupt <tilman.haupt@sap.com>
2025-03-28 12:33:42 +0100

    fix(dashboard): wrap tests with AuroraProvider

--- apps/aurora-portal/src/client/Compute/components/ComponentNavBar.test.tsx
+++ apps/aurora-portal/src/client/Compute/components/ComponentNavBar.test.tsx
@@ -1,21 +1,38 @@
 import { describe, it, vi } from "vitest"
 import { render, screen, fireEvent, act } from "@testing-library/react"
 import { ComputeNavBar } from "./ComputeNavBar"
+import { AuroraProvider } from "../../Shell/AuroraProvider"
 
 describe("ComputeNavBar", () => {
   it("renders the Compute navigation bar", async () => {
-    await act(() => render(<ComputeNavBar viewMode="list" setViewMode={vi.fn()} />))
+    await act(() =>
+      render(
+        <AuroraProvider>
+          <ComputeNavBar viewMode="list" setViewMode={vi.fn()} />
+        </AuroraProvider>
+      )
+    )
     expect(screen.getByText("Server")).toBeInTheDocument()
   })
 
   it("renders search bar with placeholder text", async () => {
-    await act(() => render(<ComputeNavBar viewMode="list" setViewMode={vi.fn()} />))
+    await act(() =>
+      render(
+        <AuroraProvider>
+          <ComputeNavBar viewMode="list" setViewMode={vi.fn()} />
+        </AuroraProvider>
+      )
+    )
     expect(screen.getAllByTestId("combobox-button").length).toBe(2)
   })
 
   it("switches to card view when clicking the Card button", async () => {
     const setViewMode = vi.fn()
-    render(<ComputeNavBar viewMode="list" setViewMode={setViewMode} />)
+    render(
+      <AuroraProvider>
+        <ComputeNavBar viewMode="list" setViewMode={setViewMode} />
+      </AuroraProvider>
+    )
 
     const cardButton = screen.getByTestId("card-view")
     await act(async () => await fireEvent.click(cardButton))
@@ -25,7 +42,11 @@ describe("ComputeNavBar", () => {
 
   it("switches to list view when clicking the List button", async () => {
     const setViewMode = vi.fn()
-    render(<ComputeNavBar viewMode="card" setViewMode={setViewMode} />)
+    render(
+      <AuroraProvider>
+        <ComputeNavBar viewMode="card" setViewMode={setViewMode} />
+      </AuroraProvider>
+    )
 
     const listButton = screen.getByTestId("list-view")
 
