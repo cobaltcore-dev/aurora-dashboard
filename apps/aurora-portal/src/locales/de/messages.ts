5a11e08a53acb6b8e48c8db1af178988a42eaa0d
TilmanHaupt <tilman.haupt@sap.com>
2025-04-04 11:40:42 +0200

    chore(dashboard): compile translations

--- apps/aurora-portal/src/locales/de/messages.ts
+++ apps/aurora-portal/src/locales/de/messages.ts
@@ -1,1 +1,4 @@
-/*eslint-disable*/import type{Messages}from"@lingui/core";export const messages=JSON.parse("{\"mpby9d\":[\"Support kontaktieren\"],\"N5vGcw\":[\"Geben Sie Ihre Zugangsdaten ein, um auf Ihr Konto zuzugreifen\"],\"r64ohk\":[\"Fehler: \",[\"error\"]],\"Z3FXyt\":[\"Lädt...\"],\"dPj4yB\":[\"In Ihr Konto einloggen\"],\"HBI6nY\":[\"Need help?\"],\"8ZsakT\":[\"Passwort\"],\"n1ekoW\":[\"Anmelden\"],\"iqUvrS\":[\"Benutzer C/D/I\"],\"Fr1JyB\":[\"Willkommen zurück, \",[\"0\"],\"!\"],\"wXGHcE\":[\"Du bist bereits angemeldet.\"],\"WfwVZU\":[\"Brauchst du Hilfe? \"]}")as Messages;
+/*eslint-disable*/ import type { Messages } from "@lingui/core"
+export const messages = JSON.parse(
+  '{"mpby9d":["Support kontaktieren"],"N5vGcw":["Geben Sie Ihre Zugangsdaten ein, um auf Ihr Konto zuzugreifen"],"r64ohk":["Fehler: ",["error"]],"Z3FXyt":["Lädt..."],"dPj4yB":["In Ihr Konto einloggen"],"HBI6nY":["Need help?"],"8ZsakT":["Passwort"],"n1ekoW":["Anmelden"],"iqUvrS":["Benutzer C/D/I"],"Fr1JyB":["Willkommen zurück, ",["0"],"!"],"wXGHcE":["Du bist bereits angemeldet."],"WfwVZU":["Brauchst du Hilfe? "]}'
+) as Messages
