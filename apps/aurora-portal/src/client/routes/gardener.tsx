bd8129e01b0f55180c839db5ec819f7641f5b714
Andreas Pfau <andreas.pfau@sap.com>
2025-05-07 14:23:13 +0200

    chore(dashboard): remove experimental code

--- apps/aurora-portal/src/client/routes/gardener.tsx
+++ apps/aurora-portal/src/client/routes/gardener.tsx
@@ -2,25 +2,6 @@ import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/gardener")({
   component: Gardener,
-  beforeLoad: async ({ context }) => {
-    const { trpcClient } = context
-    if (!trpcClient) {
-      throw new Error("TRPC client is not available")
-    }
-
-    // Fetch data or perform any necessary actions before rendering the component
-    // For example, you can fetch data from the server here
-
-    trpcClient.gardener.getClusters
-      .query()
-      .then((clusters) => {
-        console.log("Fetched clusters:", clusters)
-      })
-      .catch((error) => {
-        console.error("Error fetching clusters:", error)
-      })
-    return {}
-  },
 })
 
 function Gardener() {
