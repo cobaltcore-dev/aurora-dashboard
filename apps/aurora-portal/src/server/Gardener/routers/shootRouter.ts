4ebf8651ef443ff78292b885af0dea959242d40d
Andreas Pfau <andreas.pfau@sap.com>
2025-05-07 14:25:31 +0200

    feat(bff): add bff part for gardener (#178)
    * add bff for gardener
    
    * chore(dashboard): upgrade the k8s client version
    
    * chore(dashboard): improve gardener router
    
    * chore(dashboard): small improvements
    
    * chore(dashboard): remove experimental code

---
+++ apps/aurora-portal/src/server/Gardener/routers/shootRouter.ts
@@ -0,0 +1,43 @@
+import { publicProcedure } from "../../trpc"
+import { shootListSchema, shootItemSchema } from "../types/shoot"
+import { Cluster, convertShootListToClusters, convertShootToCluster } from "../types/cluster"
+import { client } from "../client"
+import { z } from "zod"
+
+export const shootRouter = {
+  getClusters: publicProcedure.query(async (): Promise<Cluster[]> => {
+    const parsedData = shootListSchema.safeParse(
+      await client
+        .get(`apis/core.gardener.cloud/v1beta1/namespaces/garden-${process.env.GARDENER_PROJECT}/shoots`)
+        .catch((err) => {
+          console.error("Error fetching shoots:", err.message)
+          return undefined
+        })
+    )
+
+    if (!parsedData.success) {
+      console.error("Zod Parsing Error:", parsedData.error.format())
+      return []
+    }
+
+    const clusters = convertShootListToClusters(parsedData.data.items)
+    return clusters
+  }),
+
+  getClusterByName: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .query(async ({ input }): Promise<Cluster | undefined> => {
+      const parsedData = shootItemSchema.safeParse(
+        await client.get(
+          `apis/core.gardener.cloud/v1beta1/namespaces/garden-${process.env.GARDENER_PROJECT}/shoots/${input.name}`
+        )
+      )
+
+      if (!parsedData.success) {
+        console.error("Zod Parsing Error:", parsedData.error.format())
+        return undefined
+      }
+      const cluster = convertShootToCluster(parsedData.data)
+      return cluster
+    }),
+}
