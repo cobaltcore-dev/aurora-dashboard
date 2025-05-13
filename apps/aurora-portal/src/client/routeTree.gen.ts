e4069eadaf5585eac8bd1bee516fcc7ade324452
Andreas Pfau <andreas.pfau@sap.com>
2025-05-13 16:19:41 +0200

    chore(bff): implement delete and create actions for gardener bff (#190)
    * chore(bff): implement delete and create actions for gardener bff
    
    * chore(bff): implement delete and create actions for gardener bff
    
    * chore(bff): add improve gardener routers
    
    * chore(bff): implement delete and create actions for gardener bff
    
    * chore(bff): implement delete and create actions for gardener bff

--- apps/aurora-portal/src/client/routeTree.gen.ts
+++ apps/aurora-portal/src/client/routeTree.gen.ts
@@ -11,7 +11,6 @@
 // Import Routes
 
 import { Route as rootRoute } from "./routes/__root"
-import { Route as TestImport } from "./routes/test"
 import { Route as AboutImport } from "./routes/about"
 import { Route as AuthImport } from "./routes/_auth"
 import { Route as IndexImport } from "./routes/index"
@@ -27,12 +26,6 @@ import { Route as AuthAccountsAccountIdProjectsProjectIdNetworkIndexImport } fro
 import { Route as AuthAccountsAccountIdProjectsProjectIdComputeSplatImport } from "./routes/_auth/accounts/$accountId/projects/$projectId/compute/$"
 
 // Create/Update Routes
-
-const TestRoute = TestImport.update({
-  id: "/test",
-  path: "/test",
-  getParentRoute: () => rootRoute,
-} as any)
 
 const AboutRoute = AboutImport.update({
   id: "/about",
@@ -136,13 +129,6 @@ declare module "@tanstack/react-router" {
       path: "/about"
       fullPath: "/about"
       preLoaderRoute: typeof AboutImport
-      parentRoute: typeof rootRoute
-    }
-    "/test": {
-      id: "/test"
-      path: "/test"
-      fullPath: "/test"
-      preLoaderRoute: typeof TestImport
       parentRoute: typeof rootRoute
     }
     "/_auth/aurora": {
@@ -253,7 +239,6 @@ export interface FileRoutesByFullPath {
   "/": typeof IndexRoute
   "": typeof AuthRouteWithChildren
   "/about": typeof AboutRoute
-  "/test": typeof TestRoute
   "/aurora": typeof AuthAuroraRoute
   "/auth/login": typeof AuthLoginRoute
   "/gardener": typeof GardenerIndexRoute
@@ -270,7 +255,6 @@ export interface FileRoutesByTo {
   "/": typeof IndexRoute
   "": typeof AuthRouteWithChildren
   "/about": typeof AboutRoute
-  "/test": typeof TestRoute
   "/aurora": typeof AuthAuroraRoute
   "/auth/login": typeof AuthLoginRoute
   "/gardener": typeof GardenerIndexRoute
@@ -288,7 +272,6 @@ export interface FileRoutesById {
   "/": typeof IndexRoute
   "/_auth": typeof AuthRouteWithChildren
   "/about": typeof AboutRoute
-  "/test": typeof TestRoute
   "/_auth/aurora": typeof AuthAuroraRoute
   "/auth/login": typeof AuthLoginRoute
   "/gardener/": typeof GardenerIndexRoute
@@ -307,7 +290,6 @@ export interface FileRouteTypes {
     | "/"
     | ""
     | "/about"
-    | "/test"
     | "/aurora"
     | "/auth/login"
     | "/gardener"
@@ -323,7 +305,6 @@ export interface FileRouteTypes {
     | "/"
     | ""
     | "/about"
-    | "/test"
     | "/aurora"
     | "/auth/login"
     | "/gardener"
@@ -339,7 +320,6 @@ export interface FileRouteTypes {
     | "/"
     | "/_auth"
     | "/about"
-    | "/test"
     | "/_auth/aurora"
     | "/auth/login"
     | "/gardener/"
@@ -357,7 +337,6 @@ export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
   AuthRoute: typeof AuthRouteWithChildren
   AboutRoute: typeof AboutRoute
-  TestRoute: typeof TestRoute
   AuthLoginRoute: typeof AuthLoginRoute
   GardenerIndexRoute: typeof GardenerIndexRoute
   GardenerClustersClusterNameRoute: typeof GardenerClustersClusterNameRoute
@@ -368,7 +347,6 @@ const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
   AuthRoute: AuthRouteWithChildren,
   AboutRoute: AboutRoute,
-  TestRoute: TestRoute,
   AuthLoginRoute: AuthLoginRoute,
   GardenerIndexRoute: GardenerIndexRoute,
   GardenerClustersClusterNameRoute: GardenerClustersClusterNameRoute,
@@ -386,7 +364,6 @@ export const routeTree = rootRoute._addFileChildren(rootRouteChildren)._addFileT
         "/",
         "/_auth",
         "/about",
-        "/test",
         "/auth/login",
         "/gardener/",
         "/gardener/clusters/$clusterName",
@@ -407,9 +384,6 @@ export const routeTree = rootRoute._addFileChildren(rootRouteChildren)._addFileT
     },
     "/about": {
       "filePath": "about.tsx"
-    },
-    "/test": {
-      "filePath": "test.tsx"
     },
     "/_auth/aurora": {
       "filePath": "_auth/aurora.tsx",
