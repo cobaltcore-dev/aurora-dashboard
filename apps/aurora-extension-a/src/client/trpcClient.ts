d1941bbd98a7568c065dc9ca2ac6f37328259255
Andreas Pfau <andreas.pfau@sap.com>
2025-05-28 15:39:31 +0200

    chore(core): remove old extensions

--- apps/aurora-extension-a/src/client/trpcClient.ts
+++
@@ -1,9 +0,0 @@
-import type { AppRouter } from "../server/routers"
-
-import { createAuroraTRPCClient } from "@cobaltcore-dev/aurora-sdk/client"
-
-declare const BFF_ENDPOINT: string
-
-export const trpcClient = createAuroraTRPCClient<AppRouter>(BFF_ENDPOINT)
-
-export type TrpcClient = typeof trpcClient
