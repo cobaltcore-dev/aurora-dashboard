473e212642ab97d737d0e74787d9a83d61f59d29
Andreas Pfau <andreas.pfau@sap.com>
2025-05-28 15:51:05 +0200

    chore(core): remove old extensions (#223)
    * chore(core): remove old extensions
    
    * chore(core): remove old extensions
    
    * chore(core): ignore missing envs

--- apps/aurora-extension-a/src/server/routers/trpc.ts
+++
@@ -1,9 +0,0 @@
-import { getAuroraProvider } from "@cobaltcore-dev/aurora-sdk/server"
-// You can use any variable name you like.
-// We use t to keep things simple.
-const auroraProvider = getAuroraProvider()
-
-export const auroraRouter = auroraProvider.getAuroraRouter
-export const mergeRouters = auroraProvider.getAuroraMergeRouters
-export const publicProcedure = auroraProvider.getAuroraPublicProcedure
-export const protectedProcedure = auroraProvider.getAuroraProtectedProcedure
