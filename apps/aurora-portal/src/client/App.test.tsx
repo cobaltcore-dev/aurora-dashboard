24d1318b8b54c4223baf479b0c321a9e69394ecc
Tilman <147151040+TilmanHaupt@users.noreply.github.com>
2025-06-04 13:48:42 +0200

    feat(core): introduce lingUI (#230)
    * feat(core): initalisation of LingUI
    
    * feat(core): clean not used keys in lingui
    
    * test(core): test lingui
    
    * feat(core): translate Login

---
+++ apps/aurora-portal/src/client/App.test.tsx
@@ -0,0 +1,33 @@
+import { getByText, render, act } from "@testing-library/react"
+import { i18n } from "@lingui/core"
+import { I18nProvider } from "@lingui/react"
+import { ReactNode } from "react"
+
+import { messages } from "../locales/en/messages"
+import { messages as deMessages } from "../locales/de/messages"
+import App from "./App"
+
+i18n.load({
+  en: messages,
+  de: deMessages,
+})
+
+const TestingProvider = ({ children }: { children: ReactNode }) => <I18nProvider i18n={i18n}>{children}</I18nProvider>
+
+test("Content should be translated correctly in English", () => {
+  act(() => {
+    i18n.activate("en")
+  })
+  const { getByTestId, container } = render(<App />, { wrapper: TestingProvider })
+  expect(getByTestId("welcome-title")).toBeInTheDocument()
+  expect(getByText(container, "Welcome to Aurora Dashboard")).toBeDefined()
+})
+
+test("Content should be translated correctly in German", () => {
+  act(() => {
+    i18n.activate("de")
+  })
+  const { getByTestId, container } = render(<App />, { wrapper: TestingProvider })
+  expect(getByTestId("welcome-title")).toBeInTheDocument()
+  expect(getByText(container, "Willkommen beim Aurora-Dashboard")).toBeDefined()
+})
