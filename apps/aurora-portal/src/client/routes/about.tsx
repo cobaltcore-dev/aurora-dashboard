dd85e60c0f883f2081afe280c9bae9b9a0eef338
TilmanHaupt <tilman.haupt@sap.com>
2025-06-20 17:21:03 +0200

    chore(portal): translate about

--- apps/aurora-portal/src/client/routes/about.tsx
+++ apps/aurora-portal/src/client/routes/about.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router"
+import { Trans } from "@lingui/react/macro"
 
 export const Route = createFileRoute("/about")({
   component: About,
@@ -8,85 +9,110 @@ function About() {
   return (
     <div className="min-h-screen flex items-start justify-start bg-gray-100 px-6 mt-10">
       <div className="max-w-4xl w-full">
-        <h1 className="text-5xl font-bold  mb-8">About Aurora Dashboard</h1>
-
-        <p className="text-lg  leading-relaxed">
-          Welcome to <strong>Aurora Dashboard</strong>, your next-generation cloud management solution. We are dedicated
-          to simplifying how you interact with and manage your cloud infrastructure. Designed with efficiency,
-          scalability, and usability at its core, Aurora empowers you to streamline operations and unlock the full
-          potential of your cloud resources.
-        </p>
-
-        <h2 className="text-3xl font-semibold  mt-10 mb-4">Our Mission</h2>
+        <h1 className="text-5xl font-bold mb-8">
+          <Trans>About Aurora Dashboard</Trans>
+        </h1>
         <p className="text-lg leading-relaxed">
-          At Aurora, our mission is to provide a centralized platform that unifies cloud management. We aim to simplify
-          the complexities of provisioning, configuring, and scaling resources across diverse cloud environments while
-          enabling seamless growth for your business.
+          <Trans>
+            Welcome to <strong>Aurora Dashboard</strong>, your next-generation cloud management solution. We are
+            dedicated to simplifying how you interact with and manage your cloud infrastructure. Designed with
+            efficiency, scalability, and usability at its core, Aurora empowers you to streamline operations and unlock
+            the full potential of your cloud resources.
+          </Trans>
         </p>
-
-        <h2 className="text-3xl font-semibold mt-10 mb-4">Key Features</h2>
-        <ul className="mt-4 space-y-6 text-lg ">
+        <h2 className="text-3xl font-semibold mt-10 mb-4">
+          <Trans>Our Mission</Trans>
+        </h2>
+        <p className="text-lg leading-relaxed">
+          <Trans>
+            At Aurora, our mission is to provide a centralized platform that unifies cloud management. We aim to
+            simplify the complexities of provisioning, configuring, and scaling resources across diverse cloud
+            environments while enabling seamless growth for your business.
+          </Trans>
+        </p>
+        <h2 className="text-3xl font-semibold mt-10 mb-4">
+          <Trans>Key Features</Trans>
+        </h2>
+        <ul className="mt-4 space-y-6 text-lg">
           <li className="flex items-start">
-            <span className=" font-semibold text-2xl">•</span>
+            <span className="font-semibold text-2xl">•</span>
             <p className="ml-4">
-              <strong>Unified Cloud Management:</strong> Consolidates all your cloud assets into one intuitive
-              interface.
+              <Trans>
+                <strong>Unified Cloud Management:</strong> Consolidates all your cloud assets into one intuitive
+                interface.
+              </Trans>
             </p>
           </li>
           <li className="flex items-start">
             <span className="font-semibold text-2xl">•</span>
             <p className="ml-4">
-              <strong>Effortless Resource Provisioning:</strong> Quickly provision, configure, and deploy resources like
-              servers, networks, and volumes with just a few clicks.
+              <Trans>
+                <strong>Effortless Resource Provisioning:</strong> Quickly provision, configure, and deploy resources
+                like servers, networks, and volumes with just a few clicks.
+              </Trans>
             </p>
           </li>
           <li className="flex items-start">
-            <span className=" font-semibold text-2xl">•</span>
+            <span className="font-semibold text-2xl">•</span>
             <p className="ml-4">
-              <strong>Optimized Scalability:</strong> Built for businesses of all sizes, Aurora grows with you,
-              supporting simple environments and intricate multi-cloud setups alike.
+              <Trans>
+                <strong>Optimized Scalability:</strong> Built for businesses of all sizes, Aurora grows with you,
+                supporting simple environments and intricate multi-cloud setups alike.
+              </Trans>
             </p>
           </li>
           <li className="flex items-start">
-            <span className=" font-semibold text-2xl">•</span>
+            <span className="font-semibold text-2xl">•</span>
             <p className="ml-4">
-              <strong>Enhanced Productivity:</strong> By reducing operational complexity, Aurora helps your team focus
-              on what truly matters—innovating and driving business success.
+              <Trans>
+                <strong>Enhanced Productivity:</strong> By reducing operational complexity, Aurora helps your team focus
+                on what truly matters—innovating and driving business success.
+              </Trans>
             </p>
           </li>
         </ul>
-
-        <h2 className="text-3xl font-semibold  mt-10 mb-4">Why Choose Aurora?</h2>
+        <h2 className="text-3xl font-semibold mt-10 mb-4">
+          <Trans>Why Choose Aurora?</Trans>
+        </h2>
         <p className="text-lg leading-relaxed">
-          Aurora Dashboard is more than just a tool—it's your partner in navigating the cloud. Whether you're a small
-          startup or a global enterprise, Aurora provides the flexibility, power, and simplicity you need to achieve
-          your goals.
+          <Trans>
+            Aurora Dashboard is more than just a tool—it's your partner in navigating the cloud. Whether you're a small
+            startup or a global enterprise, Aurora provides the flexibility, power, and simplicity you need to achieve
+            your goals.
+          </Trans>
         </p>
-
-        <ul className="mt-4 space-y-6 text-lg ">
+        <ul className="mt-4 space-y-6 text-lg">
           <li className="flex items-start">
             <span className="font-semibold text-2xl">•</span>
             <p className="ml-4">
-              <strong>Secure & Reliable:</strong> Your data and operations are safeguarded with enterprise-grade
-              security and robust reliability.
+              <Trans>
+                <strong>Secure & Reliable:</strong> Your data and operations are safeguarded with enterprise-grade
+                security and robust reliability.
+              </Trans>
             </p>
           </li>
           <li className="flex items-start">
             <span className="font-semibold text-2xl">•</span>
             <p className="ml-4">
-              <strong>Future-Ready:</strong> Aurora is designed to evolve with the latest trends in cloud technology,
-              ensuring your solution is always cutting-edge.
+              <Trans>
+                <strong>Future-Ready:</strong> Aurora is designed to evolve with the latest trends in cloud technology,
+                ensuring your solution is always cutting-edge.
+              </Trans>
             </p>
           </li>
         </ul>
-
-        <h2 className="text-3xl font-semibold  mt-10 mb-4">Get Involved</h2>
-        <p className="text-lg  leading-relaxed">
-          We are building Aurora Dashboard to serve you better. Your feedback is invaluable in shaping a tool that meets
-          the unique needs of businesses like yours. Stay connected and join us as we redefine cloud management.
+        <h2 className="text-3xl font-semibold mt-10 mb-4">
+          <Trans>Get Involved</Trans>
+        </h2>
+        <p className="text-lg leading-relaxed">
+          <Trans>
+            We are building Aurora Dashboard to serve you better. Your feedback is invaluable in shaping a tool that
+            meets the unique needs of businesses like yours. Stay connected and join us as we redefine cloud management.
+          </Trans>
         </p>
-
-        <p className="mt-6 text-lg ">Together, we can unlock the true potential of your cloud infrastructure.</p>
+        <p className="mt-6 text-lg">
+          <Trans>Together, we can unlock the true potential of your cloud infrastructure.</Trans>
+        </p>
       </div>
     </div>
   )
