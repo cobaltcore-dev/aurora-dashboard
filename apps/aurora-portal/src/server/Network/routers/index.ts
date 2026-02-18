e3b214ea9dadaebb4ea33ea1e2878b1e735e056d
Vladislav Schur <u.shchur@sap.com>
2026-02-18 09:07:17 +0100

    feat(bff): add floating-ip router with types

--- apps/aurora-portal/src/server/Network/routers/index.ts
+++ apps/aurora-portal/src/server/Network/routers/index.ts
@@ -1,10 +1,8 @@
 import { networkRouter } from "./networkrouter"
-import { floatingIPsRouter } from "./floatingIPsRouter"
 import { auroraRouter } from "../../trpc"
 
 export const networkRouters = {
   network: auroraRouter({
     ...networkRouter,
-    ...floatingIPsRouter,
   }),
 }
