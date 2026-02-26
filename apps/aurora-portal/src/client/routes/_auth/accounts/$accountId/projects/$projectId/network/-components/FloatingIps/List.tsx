5b1e1998d75cf9258fb61a8898b18574779ec7b2
Vladislav Schur <u.shchur@sap.com>
2026-02-26 11:45:31 +0100

    feat(aurora-portal): add floating-ip search

--- apps/aurora-portal/src/client/routes/_auth/accounts/$accountId/projects/$projectId/network/-components/FloatingIps/List.tsx
+++ apps/aurora-portal/src/client/routes/_auth/accounts/$accountId/projects/$projectId/network/-components/FloatingIps/List.tsx
@@ -1,13 +1,27 @@
+import { startTransition, useState } from "react"
 import { ListToolbar } from "@/client/components/ListToolbar"
 import { trpcReact } from "@/client/trpcClient"
 import { FloatingIpListContainer } from "./-components/FloatingIpListContainer"
 
 export const FloatingIps = () => {
-  const { data: floatingIps = [], isLoading, isError, error } = trpcReact.network.floatingIp.list.useQuery({})
+  const [searchTerm, setSearchTerm] = useState("")
+  const handleSearchChange = (term: string | number | string[] | undefined) => {
+    const searchValue = typeof term === "string" ? term : ""
+    startTransition(() => setSearchTerm(searchValue))
+  }
+
+  const {
+    data: floatingIps = [],
+    isLoading,
+    isError,
+    error,
+  } = trpcReact.network.floatingIp.list.useQuery({
+    ...(searchTerm ? { searchTerm } : {}),
+  })
 
   return (
     <div className="relative">
-      <ListToolbar />
+      <ListToolbar searchTerm={searchTerm} onSearch={handleSearchChange} />
 
       <FloatingIpListContainer floatingIps={floatingIps} isLoading={isLoading} isError={isError} error={error} />
     </div>
