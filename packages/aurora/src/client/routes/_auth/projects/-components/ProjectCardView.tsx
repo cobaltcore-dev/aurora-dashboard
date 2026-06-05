3d55ce239b39e966e56f5cd00026af75d6be83fd
TilmanHaupt <tilman.haupt@sap.com>
2026-06-05 14:21:34 +0200

    refactor(core): implement cards and delete listview

--- packages/aurora/src/client/routes/_auth/projects/-components/ProjectCardView.tsx
+++ packages/aurora/src/client/routes/_auth/projects/-components/ProjectCardView.tsx
@@ -1,6 +1,6 @@
-import { Link } from "@tanstack/react-router"
+import { useNavigate } from "@tanstack/react-router"
 import { Project } from "@/server/Project/types/models"
-import { Box, ContentHeading } from "@cloudoperators/juno-ui-components"
+import { Card } from "@cloudoperators/juno-ui-components"
 import { Trans } from "@lingui/react/macro"
 
 type ProjectCardProps = {
@@ -11,22 +11,24 @@ type ProjectCardViewProps = {
 }
 
 export function ProjectCard({ project }: ProjectCardProps) {
+  const navigate = useNavigate()
   return (
-    <Link to="/projects/$projectId" params={{ projectId: project.id }} className="block text-inherit no-underline">
-      <Box className="hover:bg-theme-background-lvl-2 min-h-50 rounded-lg p-6 shadow-md">
-        <div className="w-full">
-          <ContentHeading className="text-theme-accent">{project.name}</ContentHeading>
-
-          <p className="mt-4 line-clamp-3 pr-4 leading-relaxed">{project.description}</p>
-        </div>
-      </Box>
-    </Link>
+    <Card
+      padding
+      onClick={() => navigate({ to: "/projects/$projectId", params: { projectId: project.id } })}
+      className="min-h-50"
+    >
+      <div className="w-full">
+        <p className="text-theme-accent font-semibold">{project.name}</p>
+        <p className="mt-4 line-clamp-3 pr-4 leading-relaxed">{project.description}</p>
+      </div>
+    </Card>
   )
 }
+
 export function ProjectCardView({ projects }: ProjectCardViewProps) {
   return (
-    <div className="mx-auto h-full w-full max-w-[95vw] px-4">
-      {/* Adaptive Grid: max 3 columns, adjusts on smaller screens */}
+    <div className="w-full">
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {projects?.length ? (
           projects.map((project) => <ProjectCard key={project.id} project={project} />)
