a3891b9c5759ce3be3be18759a6ed35de53cc183
Taimoor Aslam <taimoor.aslam@sap.com>
2026-06-22 18:19:03 +0200

    fix(aurora): preserve AuroraRouter type precision and export publicProcedure

--- packages/aurora/src/server/index.ts
+++ packages/aurora/src/server/index.ts
@@ -2,6 +2,7 @@ export { createServer } from "./server"
 export type { AuroraServerConfig } from "../types"
 export {
   auroraRouter,
+  publicProcedure,
   protectedProcedure,
   projectScopedProcedure,
   domainScopedProcedure,
--- packages/aurora/src/server/routers.ts
+++ packages/aurora/src/server/routers.ts
@@ -7,15 +7,19 @@ import { serviceRouters } from "./Services/routers"
 import { auroraRouter, mergeRouters } from "./trpc"
 import type { AnyRouter } from "@trpc/server"
 
-export const buildAppRouter = (policyDir: string, extraRouters: AnyRouter[] = []) =>
+const buildBaseRouter = (policyDir: string) =>
   mergeRouters(
     auroraRouter(authRouters),
     auroraRouter(buildComputeRouters(policyDir)),
     auroraRouter(buildObjectStorageRouters(policyDir)),
     auroraRouter(projectRouters),
     auroraRouter(buildNetworkRouters(policyDir)),
-    auroraRouter(serviceRouters),
-    ...extraRouters
+    auroraRouter(serviceRouters)
   )
 
-export type AuroraRouter = ReturnType<typeof buildAppRouter>
+export type AuroraRouter = ReturnType<typeof buildBaseRouter>
+
+export const buildAppRouter = (policyDir: string, extraRouters: AnyRouter[] = []): AuroraRouter => {
+  if (extraRouters.length === 0) return buildBaseRouter(policyDir)
+  return mergeRouters(buildBaseRouter(policyDir), ...extraRouters) as unknown as AuroraRouter
+}
